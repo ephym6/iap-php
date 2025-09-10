c666b55369d5e2486e590cab75254a5a27d348c0
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-10 18:29:26 +0300

    Configuration files

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+// Site timezone
+const SITE_TIMEZONE = 'Asia/Kolkata';
+
+// Site information
+const SITE_NAME = 'IAP PHP';
+const SITE_URL = 'http://localhost/iap-php/';
+const SITE_EMAIL = 'info@iap.com';
+
+// Site language
+const SITE_LANG = 'en';
+
+// Database configuration
+const DB_TYPE = 'mysql';
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'iap_php';
