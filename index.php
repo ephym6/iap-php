63dc3d567049ab21bccfa1054bb5f226447e4da6
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-10 18:29:58 +0300

    Using config file in index.php

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 // include the class file
 include 'classes.php';
 // or use require 'classes.php';
+require 'config.php';
 
 // create instance of the class
 $sample = new Sample();
 // call the greet method
 echo $sample->greet() . "\n";
 echo $sample->week_day() . "\n";
+
+print '<br>';
+print 'Copyright &copy; 2025 - ' . SITE_NAME . ' - All rights reserved.';
+print '<br>';
