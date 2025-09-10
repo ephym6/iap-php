12ccda499ab4591245cf3da853f4c3500aee7b9e
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-10 21:03:04 +0300

    Added sign in form

--- forms/forms.php
+++ forms/forms.php
@@ -10,7 +10,19 @@ class Forms {
             <input type="email" id="email" name="email" required><br><br>
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required><br><br>
-            <?php $this->submitButton('Sign Up'); ?> <a href="login.php">Already have an account? Log in</a>
+            <?php $this->submitButton('Sign Up'); ?> <a href="/login.php">Already have an account? Log in</a>
+        </form>
+<?php
+    }
+
+    public function signIn() {
+?>
+        <form action="login.php" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <?php $this->submitButton('Log In'); ?> <a href="/index.php">Don't have an account? Sign up</a>
         </form>
 <?php
     }
