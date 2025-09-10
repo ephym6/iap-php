d3a27712739aaabd65b44037dd1594b11c0366cf
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-10 17:54:15 +0300

    sample class instantiated

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-print "hello world\n";
+// include the class file
+include 'classes.php';
+// or use require 'classes.php';
+
+// create instance of the class
+$sample = new Sample();
+// call the greet method
+echo $sample->greet() . "\n";
+echo $sample->week_day() . "\n";
