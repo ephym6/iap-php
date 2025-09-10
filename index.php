fdf6ad6c915b33b258e93738ca9ecdc72178194e
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-10 21:03:24 +0300

    Included sign up page

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-// include the class file
-include 'classes.php';
-// or use require 'classes.php';
-require 'config.php';
 
-// create instance of the class
+require 'ClassAutoLoad.php';
+
+// Class instances
 $sample = new Sample();
-// call the greet method
-echo $sample->greet() . "\n";
-echo $sample->week_day() . "\n";
+$layout = new Layouts();
+$forms = new Forms();
 
-print '<br>';
-print 'Copyright &copy; 2025 - ' . SITE_NAME . ' - All rights reserved.';
-print '<br>';
+$layout->header();
+
+$forms->signUp();
+
+$layout->footer();
