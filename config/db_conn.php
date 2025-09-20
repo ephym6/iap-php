759719152cd77d224f83e9401161f1660f803a06
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-20 13:49:23 +0300

    Commented successfull echo line

--- config/db_conn.php
+++ config/db_conn.php
@@ -15,7 +15,7 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "Connected successfully";
+    // echo "Connected successfully";
 };
 
 return $conn;
