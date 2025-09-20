eceb6c0eb8b968eab80e0b83b9f9502770fda23f
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-20 13:23:54 +0300

    Loaded database $conn in class autoload

--- ClassAutoLoad.php
+++ ClassAutoLoad.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'config/config.php';
+$conn = require 'config/db_conn.php';
 
 $directory = array('Global', 'layouts', 'forms', 'config');
 
@@ -8,7 +9,10 @@ spl_autoload_register(function ($className) use ($directory) {
     foreach ($directory as $dir) {
         $file = $dir . '/' . $className . '.php';
         if (file_exists($file)) {
-            require $file;
+            require_once $file;
+            return;
         }
     };
 });
+
+return $conn;
