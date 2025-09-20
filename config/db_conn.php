481f6aed7f81af17844514312b8cfa4a35482717
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-20 11:35:01 +0300

    Created and tested database connection

---
+++ config/db_conn.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once dirname(__DIR__) . '/config/config.php';
+
+// DATABASE CONNECTION
+$host = DB_HOST;
+$db   = DB_NAME;
+$user = DB_USER;
+$pass = DB_PASS;
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+};
