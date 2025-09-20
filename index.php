c9bc2f6ddfc34f7471d3eea96aa19c5bd41eb8a9
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-20 11:18:44 +0300

    Insantiated mail

--- index.php
+++ index.php
@@ -6,9 +6,12 @@ require 'ClassAutoLoad.php';
 $sample = new Sample();
 $layout = new Layouts();
 $forms = new Forms();
+$mail = new Mail();
 
 $layout->header();
 
 $forms->signUp();
+
+$mail->sendMail();
 
 $layout->footer();
